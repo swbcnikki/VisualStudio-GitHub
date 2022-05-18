31f17a5e9e52273d328f26de2446ee0f87eeb3a9
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-05-17 21:54:03 -0700

    Accounting Software Company Welcome

---
+++ Accounting_Sw_Co_Welcome/accountingCompanyWelcome/accountingCompanyWelcome.cs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace accountingCompanyWelcome.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to ACME Accounting Systems! Remember, we are \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
